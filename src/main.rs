45c12f769930fc5571f79c9b3c7e843bf64808f6
TheWebDevel <sathish@skcript.com>
2020-06-21 22:17:16 +0530

    multi-line code frame

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,33 @@ use std::fs::File;
 use std::io;
 use std::io::BufRead;
 
+trait Balanced {
+    /// Returns true if the brackets are balanced
+    fn count_braces(&self) -> i64;
+}
+
+impl<'a> Balanced for str {
+    fn count_braces(&self) -> i64 {
+        let mut count = 0;
+
+        for bracket in self.chars() {
+            let change = match bracket {
+                '(' => 1,
+                ')' => -1,
+                _ => 0,
+            };
+
+            count += change;
+        }
+
+        count
+    }
+}
+
+fn count_newlines(s: &str) -> usize {
+    s.as_bytes().iter().filter(|&&c| c == b'\n').count()
+}
+
 #[allow(dead_code)]
 #[allow(unused_assignments)]
 fn main() -> io::Result<()> {
@@ -11,7 +38,6 @@ fn main() -> io::Result<()> {
     let mut result = String::new();
     let call_line = 19;
     let mut start_loc: i64 = call_line - 2;
-    let mut end_loc: i64 = call_line + 3;
 
     if start_loc <= 0 {
         start_loc = 1
@@ -21,40 +47,44 @@ fn main() -> io::Result<()> {
     let mut vec_lines = Vec::new();
     vec_lines = lines.collect();
     let file_length = vec_lines.len() as i64;
-    if end_loc > file_length {
-        end_loc = file_length;
-    }
-
-    println!("\n====================");
-    println!(
-        "File Length: {file_length}
-Line: {line}
-
-Code Frame:
-Start Location: {start_loc}
-End Location: {end_loc}
-    ",
-        file_length = file_length,
-        line = call_line,
-        start_loc = start_loc,
-        end_loc = end_loc,
-    );
-    println!("====================");
 
     let mut position = start_loc;
-    let code_break = &vec_lines[(start_loc - 1) as usize..(end_loc) as usize];
-    for line in code_break {
+    let mut start_coloring = false;
+    let mut paranthesis_count = 0;
+    let code_frame = &vec_lines[(start_loc - 1) as usize..];
+    for line in code_frame {
         if let Ok(ip) = line {
             let mut formatted_line = format!("{} | {}", position, ip);
             if position == call_line {
+                paranthesis_count += ip.count_braces();
+                if paranthesis_count != 0 {
+                    start_coloring = true;
+                }
+                formatted_line = formatted_line.red().to_string();
+            } else if start_coloring {
+                paranthesis_count += ip.count_braces();
+                if paranthesis_count == 0 {
+                    start_coloring = false;
+                }
                 formatted_line = formatted_line.red().to_string();
             } else {
+                if count_newlines(&result) >= 5 {
+                    break;
+                }
                 formatted_line = formatted_line.dimmed().to_string();
+                if position > file_length {
+                    println!("{} - {}", position, file_length);
+                    break;
+                }
             }
             result.push_str(formatted_line.as_str());
             result.push_str("\n");
         }
         position += 1;
+
+        // if result.len() >= 5 {
+        //     break;
+        // }
     }
 
     println!("{}", result);
